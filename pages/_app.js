d6bae1f0a395da4a3a59e3bd1a95b4bdcddb9476
Michael Wellman <45575586+mwellman17@users.noreply.github.com>
2021-02-13 12:04:42 -0500

    Revert "mw/UI dev"

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/index.scss';
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
--- pages/dev.js
+++ pages/dev.js
@@ -1,13 +1,11 @@
-// TODO: Rename this index.js when react migration is complete
-
-import Layout from "../components/Layout"
-
-const Home = () => (
-  <Layout pageTitle="Home">
-    <div>
-      Home page
-    </div>
-  </Layout>
-);
-
-export default Home;
+// TODO(hannah): This component is just to prove to us that React is working as
+// expected. Once we've migrated over, remove this!
+const Dev = () => {
+    return(
+      <div>
+          Hello, world!
+      </div>
+    );
+  }
+  
+export default Dev;
