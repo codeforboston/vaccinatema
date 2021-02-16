c19cc8ac9eeec0b18abfc05c71da42983faecb94
Hannah <hannah@khanacademy.org>
2021-02-16 18:04:44 -0500

    Last batch of lint fixes.

--- components/subcomponents/Logo.js
+++ components/subcomponents/Logo.js
@@ -1,9 +1,11 @@
+import React from 'react';
+
 // TODO: remove "../" after promoting to root
 
 const Logo = () => (
-  <div className="logo-container" onClick={() => window.location.href = "/" }>
-    <h3><img src={"../ma_logo.png"} alt="MA logo"/>VaccinateMA</h3>
-  </div>
+    <div className="logo-container" onClick={() => window.location.href = '/' }>
+        <h3><img src={'../../ma_logo.png'} alt="MA logo"/>VaccinateMA</h3>
+    </div>
 );
 
 export default Logo;
