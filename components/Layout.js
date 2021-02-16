5dc93ce8f879d573f243321328d4c5b1f1d72314
Hannah <hannah@khanacademy.org>
2021-02-16 17:47:10 -0500

    Lint more JavaScript files.

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import Seo from './Seo';
 import NavHeader from './NavHeader';
 import Footer from './Footer';
 
 export default function Layout({
-  children,
-  pageTitle = '',
+    children,
+    pageTitle = '',
 }) {
-  return (
-    <div className="container">
-        <Seo pageTitle={pageTitle} />
-        <NavHeader />
-        <div className="main-content">
-            {children}
+    return (
+        <div className="container">
+            <Seo pageTitle={pageTitle} />
+            <NavHeader />
+            <div className="main-content">
+                {children}
+            </div>
+            <Footer />
         </div>
-        <Footer />
-    </div>
-  )
+    );
 }
+
+Layout.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ]),
+    pageTitle: PropTypes.string,
+};
