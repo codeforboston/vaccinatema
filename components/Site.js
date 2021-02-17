8792092560befd5f83bd57a2c881161a5cf35b61
Chris Snyder <christopher.snyder.m@gmail.com>
2021-02-16 20:25:33 -0500

    Remove empty class names

--- components/Site.js
+++ components/Site.js
@@ -6,16 +6,16 @@ const Site = ({data}) => (
     <div>
         <div className="site_address mb-2 flex flex-row items-center">
             <Pin />
-            <a id = "site_address_1" className="underline" href={"https://maps.google.com/?q=" + data.address} target="_blank">{data.address}</a>
+            <a id="site_address_1" className="underline" href={"https://maps.google.com/?q=" + data.address} target="_blank">{data.address}</a>
         </div>
         <div>
-            <div className="" id = "latest_report_1">Latest report: {data.lastUpdated}</div>
-            <dt className="">Details</dt>
-            <dd className="" id = "details_1">{data.populationsServed}</dd>
-            <dt className="">Availability</dt>
-            <dd className="" id = "latest_info_1">{data.vaccineAvailability}</dd>
-            <dt className="">Book Now</dt>
-            <dd className="" id = "book_1">{data.bookAppointmentInformation}</dd>
+            <div id="latest_report_1">Latest report: {data.lastUpdated}</div>
+            <dt>Details</dt>
+            <dd id="details_1">{data.populationsServed}</dd>
+            <dt>Availability</dt>
+            <dd id="latest_info_1">{data.vaccineAvailability}</dd>
+            <dt>Book Now</dt>
+            <dd id="book_1">{data.bookAppointmentInformation}</dd>
         </div>
     </div>
     </li>
