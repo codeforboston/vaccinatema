3fd80aed04f0a5e170cbfaca3240aec9425781ba
Hannah <hannah@khanacademy.org>
2021-02-20 10:12:50 -0500

    Fix link to search page in navbar.

--- components/NavHeader.js
+++ components/NavHeader.js
@@ -26,7 +26,7 @@ const NavHeader = () => {
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto"></Nav>
                     <Nav>
-                        <Nav.Link className={renderLinkClass('home')} href="/dev">Search</Nav.Link>
+                        <Nav.Link className={renderLinkClass('home')} href="/dev/search">Search</Nav.Link>
                         <Nav.Link className={renderLinkClass('eligibility')} href="/dev/eligibility">Eligibility</Nav.Link>
                         <Nav.Link className={renderLinkClass('FAQ')} href="/dev/FAQ">FAQ</Nav.Link>
                         <Nav.Link className={renderLinkClass('press')} href="/dev/press">Press</Nav.Link>
