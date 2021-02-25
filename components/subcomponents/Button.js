d18131a8f0a9d528c590bec29af969ae979f8a47
Hannah <hannah@khanacademy.org>
2021-02-24 21:58:56 -0500

    Extend button to handle both actions and links.

--- components/subcomponents/Button.js
+++ components/subcomponents/Button.js
@@ -1,28 +1,62 @@
 import React from 'react';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSearch, faLocationArrow} from '@fortawesome/free-solid-svg-icons';
 
-const Button = (props) => (
-    <button
-        className={`button-custom button-custom-${props.color}`}
-        onClick={props.onClick}
-    >
-        {props.icon && props.icon == 'search' && (
-            <FontAwesomeIcon icon={faSearch} />
-        )}
-        {props.icon && props.icon == 'location' && (
-            <FontAwesomeIcon icon={faLocationArrow} />
-        )}
-        {props.title}
-    </button>
+/**
+ * A button that triggers some action on click.
+ */
+export const ActionButton = (props) => (
+    <Button {...props} />
 );
 
-export default Button;
+/**
+ * A button that redirects to another page or website.
+ */
+export const LinkButton = (props) => (
+    <Link href={props.href} passHref>
+        <Button {...props} />
+    </Link>
+);
 
-Button.propTypes = {
+const Button = React.forwardRef((props, ref) => (
+    <div className={`button-custom button-custom-${props.color}`}>
+        <a
+            href={props.href}
+            onClick={props.onClick}
+            ref={ref}
+        >
+            {props.icon && props.icon == 'search' && (
+                <FontAwesomeIcon icon={faSearch} />
+            )}
+            {props.icon && props.icon == 'location' && (
+                <FontAwesomeIcon icon={faLocationArrow} />
+            )}
+            {props.title}
+        </a>
+    </div>
+));
+
+const sharedProps = {
     title: PropTypes.string.isRequired,
     icon: PropTypes.oneOf(['search', 'location']),
     color: PropTypes.oneOf(['gray', 'blue', 'green']).isRequired,
-    onClick: PropTypes.func.isRequired,
+};
+
+ActionButton.propTypes = {
+    ...sharedProps,
+    onClick: PropTypes.func,
+};
+
+LinkButton.propTypes = {
+    ...sharedProps,
+    href: PropTypes.string,
+};
+
+Button.displayName = 'Button';
+Button.propTypes = {
+    ...sharedProps,
+    onClick: PropTypes.func,
+    href: PropTypes.string,
 };
