43ae49390d785b488b35fd6ec31b1d888d8f580b
Hannah <hannah@khanacademy.org>
2021-03-06 12:18:12 -0500

    Add no available appointment banner.

--- components/subcomponents/ListView.js
+++ components/subcomponents/ListView.js
@@ -4,29 +4,46 @@ import PropTypes from 'prop-types';
 import SearchResult from '../SearchResult';
 import parseURLsInStrings from '../utilities/parseURLsInStrings';
 import {dateToString} from '../utilities/date-utils';
+import AvailabilityBanner from './AvailabilityBanner';
 
 const ListView = (props) => {
+    const sitesWithAvailability = props.rawSiteData.filter(
+        (site) => site.availability.length > 0
+    );
+    const sitesWithoutAvailability = props.rawSiteData.filter(
+        (site) => site.availability.length === 0
+    );
+
+    const siteDataToSearchResult = (site, i) => {
+        return (
+            <div key={i}>
+                <SearchResult
+                    name={site.name}
+                    address={site.address}
+                    siteDetails={parseURLsInStrings(site.serves)}
+                    availability={
+                        site.availability &&
+                        parseURLsInStrings(site.availability)
+                    }
+                    lastChecked={dateToString(site.lastUpdated)}
+                    bookAppointmentInfo={parseURLsInStrings(
+                        site.bookAppointmentInfo
+                    )}
+                />
+            </div>
+        );
+    };
+
     return (
         <div>
-            {props.rawSiteData.map((site, i) => {
-                return (
-                    <div key={i}>
-                        <SearchResult
-                            name={site.name}
-                            address={site.address}
-                            siteDetails={parseURLsInStrings(site.serves)}
-                            availability={
-                                site.availability &&
-                                parseURLsInStrings(site.availability)
-                            }
-                            lastChecked={dateToString(site.lastUpdated)}
-                            bookAppointmentInfo={parseURLsInStrings(
-                                site.bookAppointmentInfo
-                            )}
-                        />
-                    </div>
-                );
-            })}
+            {sitesWithAvailability.map(siteDataToSearchResult)}
+            {sitesWithoutAvailability.length > 0 && (
+                <AvailabilityBanner
+                    numSites={sitesWithoutAvailability.length}
+                    hasAvailability={false}
+                />
+            )}
+            {sitesWithoutAvailability.map(siteDataToSearchResult)}
         </div>
     );
 };
