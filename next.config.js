bbc2850b9e3c1fb45dc1039ebf405e67bcd07a14
Hannah <hannah@khanacademy.org>
2021-03-10 09:12:29 -0500

    Add comment.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ module.exports = {
     distDir: 'static/_next',
     async redirects() {
         return [
+            // The original website had a separate search page; now search
+            // functionality lives on the home page.
             {
                 source: '/search',
                 destination: '/',
