3b11475b329b1e7c7859b9021f250d4c06a56db0
hblumberg <hblumberg@users.noreply.github.com>
2021-03-17 10:16:02 -0400

    Enable empty searches. (#132)

--- pages/index.js
+++ pages/index.js
@@ -3,36 +3,50 @@ import React, {useEffect, useState} from 'react';
 import PreregisterBanner from '../components/PreregisterBanner';
 import Layout from '../components/Layout';
 import MapAndListView from '../components/MapAndListView';
-import SearchBar from '../components/SearchBar';
+import SearchBar, {ALL_AVAILABIITY} from '../components/SearchBar';
 
 const Home = () => {
     const [rawSiteData, setRawSiteData] = useState([]);
     const [mapCoordinates, setMapCoordinates] = useState(null);
 
     useEffect(
-        () => {
-            fetch('/initmap')
-                .then((response) => response.json())
-                .then((rawSiteData) => {
-                    // Before the user makes any searches, default to showing
-                    // all available sites.
-                    // TODO(hannah): Create new endpoint (or modify initmap) to
-                    // only return available sites.
-                    // TODO(hannah): Currently, in list view, the sites are
-                    // just listed alphabetically. Per discussions with Harlan,
-                    // we should be sorting them some more useful way.
-                    return rawSiteData.filter(
-                        (site) => site.availability.length > 0
-                    );
-                })
-                .then((rawSiteData) => setRawSiteData(rawSiteData))
-                .catch((err) => console.error(err));
-        },
+        // Before the user makes any searches, default to showing all available
+        // sites.
+        () => fetchSites({includeUnavailable: false}),
         // Empty array as 2nd param so function runs only on initial page load.
         []
     );
 
-    const getLocationData = (args) => {
+    const fetchSites = ({includeUnavailable}) => {
+        // Clear coordinates.
+        setMapCoordinates(null);
+
+        fetch('/initmap')
+            .then((response) => response.json())
+            .then((rawSiteData) => {
+                // TODO(hannah): Currently, in list view, the sites are
+                // just listed alphabetically. Per discussions with Harlan,
+                // we should be sorting them some more useful way.
+                if (includeUnavailable) {
+                    return rawSiteData;
+                }
+                // TODO(hannah): Create new endpoint to only return available
+                // sites.
+                return rawSiteData.filter(
+                    (site) => site.availability.length > 0
+                );
+            })
+            .then((rawSiteData) => setRawSiteData(rawSiteData))
+            .catch((err) => console.error(err));
+    };
+
+    const onSearch = (args) => {
+        if (!args.address && !args.latitude && !args.longitude) {
+            return fetchSites({
+                includeUnavailable: args.availability === ALL_AVAILABIITY,
+            });
+        }
+
         return fetch('/search_query_location', {
             method: 'post',
             headers: new Headers({'content-type': 'application/json'}),
@@ -54,7 +68,7 @@ const Home = () => {
         <Layout pageTitle="Home">
             <div>
                 <PreregisterBanner />
-                <SearchBar onSearch={getLocationData} />
+                <SearchBar onSearch={onSearch} />
                 <MapAndListView
                     mapCoordinates={mapCoordinates}
                     rawSiteData={rawSiteData}
