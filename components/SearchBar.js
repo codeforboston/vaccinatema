39bb44e02c22b371011ebc840413d8378bb397f8
hblumberg <hblumberg@users.noreply.github.com>
2021-04-07 19:48:46 -0400

    Make the search bar collapsible on mobile. (#146)

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
+import {faChevronDown, faTimes} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 import Button from '../components/subcomponents/Button';
 
 export const ALL_AVAILABIITY = 'All known vaccination sites';
 const AVAILABLE_ONLY = 'Sites with reported doses';
+const MOBILE_WIDTH = 640;
 
 const MISSING_INFO_ERROR = 'Please enter a city, town, or ZIP.';
 const GEOLOCATION_ERROR = 'Cannot figure out your location.';
@@ -14,6 +17,25 @@ const SearchBar = (props) => {
     const [availability, setAvailability] = useState(AVAILABLE_ONLY);
     const [maxMiles, setMaxMiles] = useState(null);
     const [error, setError] = useState(null);
+
+    const [isMobile, setIsMobile] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const handleWindowSizeChange = () => {
+        setIsMobile(window.innerWidth <= MOBILE_WIDTH);
+    };
+
+    // Update isMobile when the window is resized.
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        };
+    }, []);
+
+    // Update isCollapsed if isMobile changes. (The default state is collapsed 
+    // on small screens and expanded on large screens.)
+    useEffect(() => setIsCollapsed(isMobile), [isMobile]);
 
     const clearErrors = () => {
         setError(null);
@@ -77,9 +99,37 @@ const SearchBar = (props) => {
         setMaxMiles(distance === '-1' ? null : distance);
     };
 
+    const Toggle = () => (
+        <button className="toggle" onClick={() => setIsCollapsed(!isCollapsed)}>
+            {isCollapsed ? (
+                <FontAwesomeIcon icon={faChevronDown} />
+            ) : (
+                <FontAwesomeIcon icon={faTimes} />
+            )}
+        </button>
+    );
+
+    if (isCollapsed) {
+        return (
+            <div className="search-header sticky-top full-width">
+                <div className="container collapsed-container">
+                    <p>Show me vaccines near...</p>
+                    <Toggle />
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className="search-header sticky-top full-width">
             <div className="container">
+                {isMobile && (
+                    <div className="relative">
+                        <div className="toggle-container">
+                            <Toggle />
+                        </div>
+                    </div>
+                )}
                 <div className="search-header-contents">
                     {/* The "sections" help to ensure the buttons stay on the
                     same line while resizing.*/}
