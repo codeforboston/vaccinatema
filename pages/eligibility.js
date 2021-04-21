61f042777332b2165c0ae0987ee4feb519b9add9
madeleine-b <madeleine-b@users.noreply.github.com>
2021-04-21 08:45:07 -0400

    Update eligibility page with new graphic and language showing that all people age 16 and up in MA are now eligible. (#163)

--- pages/eligibility.js
+++ pages/eligibility.js
@@ -9,27 +9,24 @@ const Eligibility = () => (
         <div id="eligibility-page">
             <h1>Eligibility</h1>
             <p>
-                Massachusetts is now in vaccine eligibility Phase 2, Group 5,
-                which started on April 5, 2021. Please check all eligibility
-                requirements below and sign up for your appointment in advance
-                using the link or phone number included in the site location’s
-                details. Note that minors (16-17 years old) are only eligible to
-                receive the Pfizer vaccine, not Moderna or Janssen. Vaccination locations
-                are available only to those within active and eligible priority
-                groups as a part of the Massachusetts{' '}
+                Everyone age 16+ in Massachusetts is now eligible under{' '}
                 <a
                     href="https://www.mass.gov/info-details/covid-19-vaccine-distribution-timeline-phase-overview"
                     target="_blank"
                     rel="noreferrer"
                 >
-                    vaccine distribution timeline
-                </a>
-                . Individuals must present proof of their eligibility to receive
-                the vaccine. <b>All sites require appointments.</b>
+                    Phase 3
+                </a>,
+                which started on April 19, 2021. Some vaccination clinics may have more specific eligibility
+                requirements, so be sure to check when signing up. 
+            
+                Note that minors (16-17 years old) are only eligible to
+                receive the Pfizer vaccine, not Moderna or Janssen. <b>Most sites require appointments. </b> 
+                Clinics that are open to walk-ins will be listed as such.
             </p>
             <img
                 className="eligibility-image"
-                src={'../static/updated_eligibility_4_5.png'} 
+                src={'../static/updated_eligibility_4_19.png'} 
                 alt="COVID-19 Vaccination in MA: Phase 1, 2, and 3 Eligibility Status"
             />
         </div>
